d8e9586b090447e23383645d07ba8a4132736f12
Ruben Nic <ruben.niculcea@gmail.com>
2026-08-06 13:08:28 -0400

    have vite generate the 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: ['index.html', '404.html'],
+    },
+  },
   server: {
     open: true,
     port: process.env.PORT ? Number(process.env.PORT) : 5173,
